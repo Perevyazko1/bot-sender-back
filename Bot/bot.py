e19c9643498779a089dcc44b9606f6fe6283cc8d
Perevyazko <a.perevyazko@gmail.com>
2024-03-25 20:23:03 +0300

    setting bot

--- Bot/bot.py
+++ Bot/bot.py
@@ -5,9 +5,11 @@ import re
 import aioschedule
 import redis
 import requests
+import random
 from aiogram import Bot, Dispatcher
 from aiogram import types
 from aiogram.types import ParseMode
+from aiogram.dispatcher.filters import IsReplyFilter, BoundFilter
 from aiogram.types import WebAppInfo
 from aiogram.utils import executor
 from aiogram.utils.exceptions import (MessageCantBeDeleted,
@@ -21,6 +23,7 @@ redis_host = 'redis'
 redis_port = 6379
 redis_db = 0
 redis_client = redis.Redis(host=redis_host, port=redis_port, db=redis_db)
+
 
 async def check_and_send_notifications():
     while True:
@@ -36,14 +39,12 @@ async def check_and_send_notifications():
             await asyncio.sleep(1)
 
 
-def clear_text(a):
-    return re.sub(r"['\[\]()\,]", "", str(a))
-
 @dp.message_handler(commands="start")
 async def start(message: types.Message):
     await message.answer(f'Привет {message.from_user.full_name} \n\n' '<i>Этот бот предназначен для отправки заданных '
                          'сообщений в ТОЛЬКО в группу по расписанию.\n\n'
-                         'Для в заимодействия с ботом необходимо добавить его в группу и назначить администратором.</i>\n\n'
+                         'Для взаимодействия с ботом необходимо добавить его в группу и назначить '
+                         'администратором.</i>\n\n'
                          '<b><u>ЗАДАЧИ МОЖЕТ ВЫСТАВЛЯТЬ ТОЛЬКО АДМИНИСТРАТОР ЧАТА</u></b>',
                          parse_mode=types.ParseMode.HTML, disable_notification=True)
 
@@ -52,46 +53,54 @@ async def start(message: types.Message):
 async def task(message: types.Message):
     group = message.chat.id
     markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("Работа с задачами", url=f"{os.getenv('URL_FRONT')}{group}", callback_data='copy_text'))
+    markup.add(types.InlineKeyboardButton("Работа с задачами", url=f"{os.getenv('URL_FRONT')}{group}",
+                                          callback_data='copy_text'))
     # markup.add(types.InlineKeyboardButton("открыть страницу",web_app=WebAppInfo(url=f"https://perevyazko1.github.io/testprojectwebappbot/{group}")))
-    await message.answer(f'Нажмите кнопку', reply_markup=markup,parse_mode=ParseMode.MARKDOWN)
+    await message.answer(f'Нажмите кнопку', reply_markup=markup, parse_mode=ParseMode.MARKDOWN)
+
+    if message.text == '/task_create@CREATOR_TASK_FOR_CHAT_BOT':
+        try:
+            await message.delete()
+        except MessageCantBeDeleted:
+            await asyncio.sleep(1)
 
 
 @dp.message_handler()
 async def handle_message(message: types.Message):
     modified_text = re.sub(r"(https://www\.)(instagram\.com/.+)", r"\1dd\2", message.text)
     if modified_text != message.text:
-       await message.reply(modified_text)
-
-# @dp.message_handler(commands="list_task")
-# async def button_task(message: types.Message):
-#     marcup = types.ReplyKeyboardMarkup()
-#     marcup.add(types.KeyboardButton("открыть страницу", webapp="https://perevyazko1.github.io/botsenderfront"))
-
-
-
-@dp.message_handler()
-async def delete_command(message: types.Message):
-    if message.text == '/start@CREATOR_TASK_FOR_CHAT_BOT' or message.text == '/task@CREATOR_TASK_FOR_CHAT_BOT' \
-            or message.text == '/delete_task@CREATOR_TASK_FOR_CHAT_BOT' or message.text == '/list_task@CREATOR_TASK_FOR_CHAT_BOT':
+        await message.answer(modified_text)
         try:
             await message.delete()
         except MessageCantBeDeleted:
             await asyncio.sleep(1)
 
+
+class ReplyFilterBot(BoundFilter):
+    async def check(self, msg: types.Message):
+        try:
+            if msg.reply_to_message.from_user.id == bot.id:
+                return True
+        except Exception:
+            pass
+
+
+@dp.message_handler(ReplyFilterBot())
+async def joke_answer(message: types.Message):
+    await message.answer(text=random.choice(os.getenv('ANSWER_PHRASE')))
+
+
 @dp.message_handler()
 async def load_task(chat_id_group, text_task):
-    print(text_task)
     try:
         await dp.bot.send_message(chat_id=chat_id_group, text=text_task)
         await asyncio.sleep(1)
     except BotKicked:
         await asyncio.sleep(1)
 
+
 async def send_task():
-
     url = f"{os.getenv('SERVER_IP')}/app/get_all_task/"
-
 
     response = requests.get(url)
 
@@ -108,10 +117,10 @@ async def send_task():
             'Воскресенье': aioschedule.every().sunday,
         }
         for task in sql_tasks:
-            day_of_week = task.get('day_of_week','Ежедневно')
+            day_of_week = task.get('day_of_week', 'Ежедневно')
             if day_of_week in day_mapping:
                 day_mapping[day_of_week].at(time_str=task.get('time')).do(load_task, task.get('chat_id'),
-                                                                               task.get('task')).tag(f'{task.get("id")}')
+                                                                          task.get('task')).tag(f'{task.get("id")}')
             else:
                 print(f"Unsupported day of the week: {day_of_week}")
     else:
@@ -134,9 +143,9 @@ async def send_task():
         if create_tasks:
             _, task_json = create_tasks
             task = json.loads(task_json.decode('utf-8'))
-            print(task)
             day_of_week = task.get('day_of_week')
-            day_mapping[day_of_week].at(time_str=task.get('time')).do(load_task, task.get('chat_id'),task.get('task')).tag(f'{task.get("id")}')
+            day_mapping[day_of_week].at(time_str=task.get('time')).do(load_task, task.get('chat_id'),
+                                                                      task.get('task')).tag(f'{task.get("id")}')
         if delete_tasks:
             _, task_json = delete_tasks
             task = json.loads(task_json.decode('utf-8'))
@@ -147,7 +156,6 @@ async def send_task():
 
         await aioschedule.run_pending()
         await asyncio.sleep(1)
-
 
 
 async def on_startup(dp):
